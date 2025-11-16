e29f59248d57e9576d0db0142b3b08c6cf497fb5
preettypeanuts <mrafly212001@gmail.com>
2025-11-16 22:00:19 +0700

    update mega menu

--- src/components/mega-services.jsx
+++ src/components/mega-services.jsx
@@ -1,6 +1,6 @@
 import { slugify } from "@/lib/slugify"
 import { productsData } from "@/system"
-import { ChevronRight } from "lucide-react"
+import { ChevronRight, Circle, Dot, Flower2 } from "lucide-react"
 import Link from "next/link"
 
 export const MegaServices = () => {
@@ -9,34 +9,41 @@ export const MegaServices = () => {
     return (
         <>
             <div className="grid grid-cols-10">
-                <section className="col-span-8 grid grid-cols-4 pb-10 gap-2">
-                    {divisions.map(division => (
-                        <div
-                            className="border border-neutral-500/20 rounded-lg px-1 py-1"
-                            key={division}
-                        >
+                <section className="col-span-8 space-y-3">
+                    <div className="uppercase text-xs font-bold opacity-50">
+                        Products
+                    </div>
+                    <div className="grid grid-cols-4 pb-10">
+                        {divisions.map((division, idx) => (
                             <Link
                                 href={`/${slugify(division)}`}
-                                className="flex items-center justify-between bg-darkColor/5 dark:bg-lightColor/5 rounded-sm px-3 py-2 text-mainColor hover:bg-mainColor/20 dark:hover:bg-mainColor/20 duration-300">
-                                <h2 className="font-bold text-sm uppercase ">
-                                    {division}
-                                </h2>
-                                <ChevronRight className="size-5.5 opacity-70" />
+                                className="bg-white dark:bg-black border p-5 rounded-lg duration-300 hover:invert group"
+                                key={division}
+                            >
+                                <div className="flex items-start justify-between mb-10 gap-20">
+                                    <div className="text-2xl text-mainColor font-light leading-tight group-hover:text-sky-600">
+                                        {division}
+                                    </div>
+                                    <p className="text-xs p-1">
+                                        0{idx + 1}.
+                                    </p>
+                                </div>
+
+                                <div className="space-y-0.5">
+                                    {productsData
+                                        .filter(item => item.division === division)
+                                        .map(product => (
+                                            <div
+                                                className="text-xs line-clamp-1"
+                                                key={product.id}
+                                            >
+                                                {product.productName}
+                                            </div>
+                                        ))}
+                                </div>
                             </Link>
-                            <div className="opacity-70 text-sm py-2 px-3 space-y-1">
-                                {productsData
-                                    .filter(item => item.division === division)
-                                    .map(product => (
-                                        <div
-                                            key={product.id}
-                                            className="border-b border-neutral-500/20 pb-1.5"
-                                        >
-                                            {product.productName}
-                                        </div>
-                                    ))}
-                            </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </section>
                 <section className="col-span-2">
 
